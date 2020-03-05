78bad1eaa1ae6457ac2f008fe53ca2c5ac9aeb50
DengZhiGuo <bobdeng@163.com>
2020-03-05 14:48:24 +0800

    refactor

--- tenant-domain/src/main/java/cn/bobdeng/tenant/domain/events/TenantEvents.java
+++ tenant-domain/src/main/java/cn/bobdeng/tenant/domain/events/TenantEvents.java
@@ -3,5 +3,5 @@ package cn.bobdeng.tenant.domain.events;
 public class TenantEvents {
     public static final String NEW_TENANT_EVENT = "new_tenant";
     public static final String REMOVE_TENANT_EVENT = "remove_tenant";
-    public static final String RENEW_CONTRACT = "renew_contract";
+    public static final String RENEW_CONTRACT = "renew_tenant_contract";
 }
