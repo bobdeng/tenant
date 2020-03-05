06c179505cf648ce28bcbf293d29795a1eaf205c
DengZhiGuo <bobdeng@163.com>
2020-03-05 16:38:39 +0800

    refactor

--- tenant-domain/src/main/java/cn/bobdeng/tenant/domain/events/RemoveTenantEvent.java
+++ tenant-domain/src/main/java/cn/bobdeng/tenant/domain/events/RemoveTenantEvent.java
@@ -6,8 +6,10 @@ import lombok.Data;
 @Data
 public class RemoveTenantEvent {
     private long id;
+    private int apartment;
 
     public RemoveTenantEvent(Tenant tenant) {
         this.id = tenant.getId();
+        this.id = tenant.getRentContact().getApartmentId();
     }
 }
